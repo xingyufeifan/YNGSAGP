48116e2046f41c401cfceab12ba3e1ec27cef5a3
xingyufeifan <634846539@qq.com>
2017-11-13 17:26:19 +0800

    添加依赖库

--- app/src/main/java/com/nandi/yngsagp/MyApp.java
+++ app/src/main/java/com/nandi/yngsagp/MyApp.java
@@ -2,7 +2,12 @@ package com.nandi.yngsagp;
 
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
 
+import com.alibaba.sdk.android.push.CloudPushService;
+import com.alibaba.sdk.android.push.CommonCallback;
+import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.cookie.CookieJarImpl;
 import com.zhy.http.okhttp.cookie.store.PersistentCookieStore;
@@ -17,10 +22,11 @@ import okhttp3.OkHttpClient;
  */
 
 public class MyApp extends Application {
-
+    private static final String TAG = "MyApplication";
     @Override
     public void onCreate() {
         super.onCreate();
+        initCloudChannel(getApplicationContext());
         CookieJarImpl cookieJar = new CookieJarImpl(new PersistentCookieStore(getApplicationContext()));
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .addInterceptor(new LoggerInterceptor("网络请求"))
@@ -31,5 +37,22 @@ public class MyApp extends Application {
         OkHttpUtils.initClient(okHttpClient);
     }
 
-
+    /**
+     * 初始化云推送通道
+     * @param applicationContext
+     */
+    private void initCloudChannel(Context applicationContext) {
+        PushServiceFactory.init(applicationContext);
+        CloudPushService pushService = PushServiceFactory.getCloudPushService();
+        pushService.register(applicationContext, new CommonCallback() {
+            @Override
+            public void onSuccess(String response) {
+                Log.d(TAG, "init cloudchannel success");
+            }
+            @Override
+            public void onFailed(String errorCode, String errorMessage) {
+                Log.d(TAG, "init cloudchannel failed -- errorcode:" + errorCode + " -- errorMessage:" + errorMessage);
+            }
+        });
+    }
 }
