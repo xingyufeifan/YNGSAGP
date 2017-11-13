0131f059eafe06bb1d0b11b81234ec195eb55aa1
qingsong <1154046205@qq.com>
2017-11-13 12:33:02 +0800

    登录

---
+++ app/src/main/java/com/nandi/yngsagp/LoginActivity.java
@@ -0,0 +1,208 @@
+package com.nandi.yngsagp;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+
+
+import com.lzy.okgo.OkGo;
+import com.nandi.yngsagp.utils.SharedUtils;
+import com.nandi.yngsagp.utils.ToastUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @BindView(R.id.et_mobile)
+    EditText etMobile;
+    @BindView(R.id.iv_clean_phone)
+    ImageView ivCleanPhone;
+    @BindView(R.id.et_password)
+    EditText etPassword;
+    @BindView(R.id.clean_password)
+    ImageView cleanPassword;
+    @BindView(R.id.iv_show_pwd)
+    ImageView ivShowPwd;
+    @BindView(R.id.btn_login)
+    Button btnLogin;
+    @BindView(R.id.scrollView)
+    ScrollView scrollView;
+    @BindView(R.id.root)
+    RelativeLayout root;
+
+    private int screenHeight = 0;//屏幕高度
+    private int keyHeight = 0; //软件盘弹起后所占高度
+    private String pwd;
+    private String mobile;
+    private ProgressDialog progressDialog;
+    private String isLogin = "-1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        isLogin = (String) SharedUtils.getShare(this, "isLogin", "");
+        if ("1".equals(isLogin)) {
+
+            finish();
+        }else {
+            intiView();
+            initListener();
+
+        }
+    }
+
+    private void intiView() {
+        screenHeight = this.getResources().getDisplayMetrics().heightPixels; //获取屏幕高度
+        keyHeight = screenHeight / 4;//弹起高度为屏幕高度的1/3
+        //dialog
+        progressDialog = new ProgressDialog(this, ProgressDialog.STYLE_SPINNER);
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setMessage("正在登录...");
+    }
+
+    private void initListener() {
+        ivCleanPhone.setOnClickListener(this);
+        cleanPassword.setOnClickListener(this);
+        ivShowPwd.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+        etMobile.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!TextUtils.isEmpty(s) && ivCleanPhone.getVisibility() == View.GONE) {
+                    ivCleanPhone.setVisibility(View.VISIBLE);
+                } else if (TextUtils.isEmpty(s)) {
+                    ivCleanPhone.setVisibility(View.GONE);
+                }
+            }
+        });
+        etPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!TextUtils.isEmpty(s) && cleanPassword.getVisibility() == View.GONE) {
+                    cleanPassword.setVisibility(View.VISIBLE);
+                } else if (TextUtils.isEmpty(s)) {
+                    cleanPassword.setVisibility(View.GONE);
+                }
+                if (s.toString().isEmpty())
+                    return;
+                if (!s.toString().matches("[A-Za-z0-9]+")) {
+                    String temp = s.toString();
+                    s.delete(temp.length() - 1, temp.length());
+                    etPassword.setSelection(s.length());
+                }
+            }
+        });
+        scrollView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+        findViewById(R.id.root).addOnLayoutChangeListener(new ViewGroup.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+              /* old是改变前的左上右下坐标点值，没有old的是改变后的左上右下坐标点值
+              现在认为只要控件将Activity向上推的高度超过了1/3屏幕高，就认为软键盘弹起*/
+                if (oldBottom != 0 && bottom != 0 && (oldBottom - bottom > keyHeight)) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            scrollView.smoothScrollTo(0, scrollView.getHeight());
+                        }
+                    }, 0);
+
+
+                } else if (oldBottom != 0 && bottom != 0 && (bottom - oldBottom > keyHeight)) {
+                    //键盘收回后，logo恢复原来大小，位置同样回到初始位置
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            scrollView.smoothScrollTo(0, scrollView.getHeight());
+                        }
+                    }, 0);
+
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        pwd = etPassword.getText().toString();
+        mobile = etMobile.getText().toString();
+        int id = v.getId();
+        switch (id) {
+            case R.id.iv_clean_phone:
+                etMobile.setText("");
+                break;
+            case R.id.clean_password:
+                etPassword.setText("");
+                break;
+            case R.id.iv_show_pwd:
+                if (etPassword.getInputType() != InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                    etPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    ivShowPwd.setImageResource(R.mipmap.ic_visibly);
+                } else {
+                    etPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    ivShowPwd.setImageResource(R.mipmap.ic_visibly_up);
+                }
+                if (!TextUtils.isEmpty(pwd))
+                    etPassword.setSelection(pwd.length());
+                break;
+            case R.id.btn_login:
+
+                if (TextUtils.isEmpty(mobile)) {
+                    ToastUtils.showShort(this, "请输入账号");
+                } else if (TextUtils.isEmpty(pwd)) {
+                    ToastUtils.showShort(this, "请输入密码");
+                } else if (SharedUtils.containsShare(this, "IP") && SharedUtils.containsShare(this, "PORT")) {
+                    progressDialog.show();
+                } else {
+                    ToastUtils.showShort(this, "请设置相应IP和端口");
+                }
+                break;
+        }
+    }
+
+
+
+}
