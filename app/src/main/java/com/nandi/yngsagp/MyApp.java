67fa64bffe4a9cb4887dc357224940f3ee4a8b08
xingyufeifan <634846539@qq.com>
2017-11-13 14:37:58 +0800

    添加依赖库

--- app/src/main/java/com/nandi/yngsagp/MyApp.java
+++ app/src/main/java/com/nandi/yngsagp/MyApp.java
@@ -3,6 +3,15 @@ package com.nandi.yngsagp;
 
 import android.app.Application;
 
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.cookie.CookieJarImpl;
+import com.zhy.http.okhttp.cookie.store.PersistentCookieStore;
+import com.zhy.http.okhttp.log.LoggerInterceptor;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
 /**
  * Created by qingsong on 2017/6/30.
  */
@@ -12,6 +21,14 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        CookieJarImpl cookieJar = new CookieJarImpl(new PersistentCookieStore(getApplicationContext()));
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new LoggerInterceptor("网络请求"))
+                .cookieJar(cookieJar)
+                .connectTimeout(1000* 30L, TimeUnit.MILLISECONDS)
+                .readTimeout(1000 * 30L, TimeUnit.MILLISECONDS)
+                .build();
+        OkHttpUtils.initClient(okHttpClient);
     }
 
 
