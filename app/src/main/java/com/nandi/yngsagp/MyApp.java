5f74c36b6f576a0933615e6d94bca7cd3075492f
qingsong <1154046205@qq.com>
2017-11-13 14:03:13 +0800

    登录1

--- app/src/main/java/com/nandi/yngsagp/MyApp.java
+++ app/src/main/java/com/nandi/yngsagp/MyApp.java
@@ -2,26 +2,12 @@ package com.nandi.yngsagp;
 
 
 import android.app.Application;
-import android.support.annotation.NonNull;
-
-import com.lzy.okgo.OkGo;
-import com.lzy.okgo.cache.CacheEntity;
-import com.lzy.okgo.cache.CacheMode;
-import com.lzy.okgo.interceptor.HttpLoggingInterceptor;
-
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-
-import okhttp3.OkHttpClient;
-
 
 /**
  * Created by qingsong on 2017/6/30.
  */
 
 public class MyApp extends Application {
-    public static final String TAG = "Gdims";
-    private static final int Time = 10000;
 
     @Override
     public void onCreate() {
